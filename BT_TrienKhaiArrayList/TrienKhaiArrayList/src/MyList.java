1adad409cf137022e96219bdfb827d9f829a7c60
Dat Bong <Datbong2801@>
2020-11-23 08:58:32 +0700

    new

---
+++ BT_TrienKhaiArrayList/TrienKhaiArrayList/src/MyList.java
@@ -0,0 +1,13 @@
+public class MyList<E> {
+    private int size =0;
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object elenmet[];
+
+    public MyList(){}
+    public MyList(int capacity){
+    }
+    public MyList(int index, E element){
+
+    }
+
+}
